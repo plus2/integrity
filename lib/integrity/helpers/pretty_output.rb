1fb9d03c4a6e6c511d8bd9160ec95b91aa2c9948
lachie <lachiec@gmail.com>
2010-05-24 13:56:08 +1000

    more advanced ansi colour parsing.
    There's enough to support cucumber's colouring
    but it should be simple enough to extend as required.

--- lib/integrity/helpers/pretty_output.rb
+++ lib/integrity/helpers/pretty_output.rb
@@ -9,14 +9,7 @@ module Integrity
       end
 
       def bash_color_codes(string)
-        string.gsub("\e[0m", '</span>').
-          gsub("\e[31m", '<span class="color31">').
-          gsub("\e[32m", '<span class="color32">').
-          gsub("\e[33m", '<span class="color33">').
-          gsub("\e[34m", '<span class="color34">').
-          gsub("\e[35m", '<span class="color35">').
-          gsub("\e[36m", '<span class="color36">').
-          gsub("\e[37m", '<span class="color37">')
+        AnsiToHtml.new(string).to_s
       end
 
       def pretty_date(date_time)
@@ -44,4 +37,111 @@ module Integrity
       end
     end
   end
+end
+
+class AnsiParser
+  def initialize(delegate)
+    @delegate = delegate
+    @state_changes = 0
+
+    @reset_state = {
+      :fg => 37,
+      :bg => 30,
+      :bold => false
+    }
+
+    @state = @reset_state.dup
+  end
+
+  def reset_state!
+    @delegate.state_reset(@state_changes)
+
+    @state_changes = 0
+    @state = @reset_state.dup
+  end
+
+  def update_state(changes)
+    new_state = @state.merge(changes)
+    if new_state != @state
+      @state = new_state
+      @state_changes += 1
+
+      @delegate.state_updated(@state,changes)
+    end
+  end
+
+  def add_code(number)
+    number = number.to_i
+    case number
+    when 0
+      reset_state!
+    when 1
+      update_state(:bold => true)
+    when 22
+      update_state(:bold => false)
+    when 30..37,90..97
+      update_state(:fg => number)
+    when 40..47,100..107
+      update_state(:bg => number)
+    else
+      # unhandled
+    end
+  end
+
+  def add_text(text)
+    @delegate.text_added(text)
+  end
+
+  AnsiPre  = %r{^([^\e]*)(?=\e\[\d+m)}
+  AnsiScan = %r{\e\[(\d+)m([^\e]*)}
+  def parse(text)
+    text = text.gsub('&', '&amp;').gsub('"', '&quot;').gsub('<', '&lt;').gsub('>', '&gt;')
+
+    if text.include?("\e")
+      text.sub!(AnsiPre,'')
+      pre = $1
+      add_text(pre)
+
+      text.scan(AnsiScan).each {|(code,text)| add_code(code); add_text(text)}
+    else
+      @delegate.buffer = text
+    end
+  end
+end
+
+class AnsiToHtml
+  attr_accessor :buffer
+  
+  def initialize(input)
+    @input = input
+    @buffer = ""
+    @opened = false
+  end
+
+  def to_s
+    AnsiParser.new(self).parse(@input)
+    @buffer
+  end
+
+  def text_added(text)
+    @buffer += text
+  end
+
+  def state_reset(depth)
+    @buffer += "</span>" if @opened
+    @opened = false
+  end
+
+  def state_updated(state,changes)
+    @buffer += "</span>" if @opened
+    @opened = true
+    @buffer += "<span class='#{state_to_class(state) * ' '}'>"
+  end
+
+  def state_to_class(state)
+    classes = ["ansi-fg-#{state[:fg]}", "ansi-bg-#{state[:bg]}"]
+    classes << 'ansi-bold' if state[:bold]
+    classes
+  end
+
 end
